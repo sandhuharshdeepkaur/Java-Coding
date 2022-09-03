9e834d01c2c71e8fb4289472c6b121f1d1384009
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-03 23:57:01 +0530

    JavaCummit

---
+++ Java/AdPattern.java
@@ -0,0 +1,41 @@
+package Java;
+
+public class AdPattern {
+
+    public static void butterfly(int n){
+
+        for (int i = 1; i <= n; i++){                       // 1st Half
+            
+            for (int j = 1; j <= i; j++){                  // Stars
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2*(n-i); j++){          // Spaces
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){                // Stars
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--){                  // 2nd Half
+            
+            for(int j = 1; j <= i; j++){               // Stars
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2*(n-i); j++){       // Spaces
+                System.out.print(" ");
+            }
+            for(int j = 1; j <=i; j++){               // Stars
+                System.out.print("*"); 
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        butterfly(5);
+    }
+    
+
+}
