dd53644748374d4a69a46559af0d811c12811e9e
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-03 17:06:53 +0530

    JavaCommit

--- Java/HollowRec.java
+++ Java/HollowRec.java
@@ -5,20 +5,20 @@ package Java;
 
 public class HollowRec{
 
-    public static void main(String args[]){
-        Hollow_Rec(4,5);
+    public static void main(String args[]){                                            // Main Function
+        Hollow_Rec(4,5);                                         // Calling the Method with input
     }
-    public static void Hollow_Rec(int TotalRows, int TotalCols){
+    public static void Hollow_Rec(int TotalRows, int TotalCols){                     // Method to create Hollow Rectangle
 
-        for (int i = 1; i <= TotalRows; i++){
-            for (int j = 1; j <= TotalCols; j++){
-                if (i == 1 || i == TotalRows || j == 1 || j == TotalCols){
-                    System.out.print("*");
+        for (int i = 1; i <= TotalRows; i++){                                       // Outer loop for Lines
+            for (int j = 1; j <= TotalCols; j++){                                  // Inner loop for the Stars
+                if (i == 1 || i == TotalRows || j == 1 || j == TotalCols){        // Condition for Hollow rectangle
+                    System.out.print("*");                                     // Printing the stars
                 }else{
-                    System.out.print(" ");
+                    System.out.print(" ");                                    // Printing hollow space  
                 }
             }
-            System.out.println();
+            System.out.println();                                              // For Next line
         }
     }
 } 
