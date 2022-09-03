d096ccb6d03663f4eae49f23b27196dd43d71131
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-03 18:39:53 +0530

    JavaCummit

--- Java/AdvancePatterns.java
+++ Java/AdvancePatterns.java
@@ -6,7 +6,9 @@ package Java;
 public class AdvancePatterns{
 
     public static void main(String args[]){                                            // Main Function
+        
         Hollow_Rec(4,5);                                         // Calling the Method with input
+        
         Holinvpattern(4);
     }
     
@@ -14,13 +16,16 @@ public class AdvancePatterns{
     public static void Hollow_Rec(int TotalRows, int TotalCols){                     // Method to create Hollow Rectangle
 
         for (int i = 1; i <= TotalRows; i++){                                       // Outer loop for Lines
+            
             for (int j = 1; j <= TotalCols; j++){                                  // Inner loop for the Stars
+                
                 if (i == 1 || i == TotalRows || j == 1 || j == TotalCols){        // Condition for Hollow rectangle
                     System.out.print("*");                                     // Printing the stars
                 }else{
                     System.out.print(" ");                                    // Printing hollow space  
                 }
             }
+
             System.out.println();                                              // For Next line
         }
     }
@@ -30,13 +35,17 @@ public class AdvancePatterns{
 
 
     public static void Holinvpattern(int n){
+
         for (int i = 1; i <= n; i++){                                      // Outer Loop for lines
+            
             for (int j = 1; j <= n-i; j++){                               // Inner Loop for Spaces
             System.out.print(" ");                                     // Printing the Spaces
             }
+
             for (int j = 1; j <= i; j++){                               // Inner Loop for Stars
             System.out.print("*");                                   // Printing the Stars
             }
+
             System.out.println();                                      // Next line
         }
     }
