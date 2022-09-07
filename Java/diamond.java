ca20a77ef2e55b068cdaa141de0808b513ad0649
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-07 16:08:23 +0530

    JavaPractice

---
+++ Java/diamond.java
@@ -0,0 +1,40 @@
+package Java;
+
+// Program to print diamond pattern of stars
+
+public class diamond{
+
+public static void diamondi(int n){
+
+
+    for (int i =1; i <= n; i++){                  // 1st half
+
+        for (int j = 1; j <= (n-i); j++){        // For spaces
+            System.out.print(" ");
+        }
+
+        for(int j = 1; j <= (2*i) - 1; j++){    // For stars
+            System.out.print("*");
+        }
+        System.out.println();                   // Next Line
+    }
+
+    for (int i = n; i >= 1; i--){                // 2nd half
+
+        for (int j = 1; j <= (n-i); j++){        // For Spaces
+            System.out.print(" ");
+        }
+
+        for (int j = 1; j <= (2*i)- 1; j++){     // For Stars
+            System.out.print("*");
+        }
+        System.out.println();                    // Next Line
+    }
+}
+
+
+    public static void main(String args[]){
+
+        diamondi(5);
+    }
+}
