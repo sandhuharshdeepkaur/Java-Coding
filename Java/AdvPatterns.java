871bac16aec0081a13e489d70a8f1d19d56a3f98
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-08 16:30:23 +0530

    JavaPractice

--- Java/AdvPatterns.java
+++ Java/AdvPatterns.java
@@ -1,6 +1,9 @@
 package Java;
 
+
 public class AdvPatterns {
+
+    // Program to print Pyramid numbers
 
     public static void numpy(int n){
 
@@ -15,8 +18,36 @@ public class AdvPatterns {
             System.out.println();
         }
     }
+
+
+    // Program to print palindromic pattern with numbers
+
+    public static void palinpattern(int n){
+
+        for (int i = 1; i <= n; i++){
+
+            for (int j = 1; j <= n-i; j++){                     // For Spaces
+                System.out.print(" ");
+            }
+
+            for(int j = i; j >= 1; j--){                       // For Descending
+                System.out.print(j);
+            }
+            
+            for(int j = 2; j <= i; j++){                      // For Ascending
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String args[]){
     
         numpy(9);
+
+        palinpattern(5);
 }
 }
