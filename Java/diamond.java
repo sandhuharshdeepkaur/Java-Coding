e8ab0af39c877c43bc3f0996ca5237659d4de284
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-08 14:29:54 +0530

    JavaPractice

--- Java/diamond.java
+++ Java/diamond.java
@@ -7,34 +7,51 @@ public class diamond{
 public static void diamondi(int n){
 
 
-    for (int i =1; i <= n; i++){                  // 1st half
+    for (int i =1; i <= n; i++){                         // 1st half
 
-        for (int j = 1; j <= (n-i); j++){        // For spaces
+        for (int j = 1; j <= (n-i); j++){               // For spaces
             System.out.print(" ");
         }
 
-        for(int j = 1; j <= (2*i) - 1; j++){    // For stars
+        for(int j = 1; j <= (2*i) - 1; j++){           // For stars
             System.out.print("*");
         }
-        System.out.println();                   // Next Line
+        System.out.println();                         // Next Line
     }
 
-    for (int i = n; i >= 1; i--){                // 2nd half
+    for (int i = n; i >= 1; i--){                    // 2nd half
 
-        for (int j = 1; j <= (n-i); j++){        // For Spaces
-            System.out.print(" ");
+        for (int j = 1; j <= (n-i); j++){            // For Spaces
+            System.out.print(" "); 
         }
 
-        for (int j = 1; j <= (2*i)- 1; j++){     // For Stars
+        for (int j = 1; j <= (2*i)- 1; j++){         // For Stars
             System.out.print("*");
         }
-        System.out.println();                    // Next Line
+        System.out.println();                        // Next Line
     }
 }
 
+// Program for printing number pyramid
+
+public static void numpy(int n){
+
+    for (int i = 1; i <= n; i++){                       // Lines
+        for(int j = 1; j <= (n-i); j++){               // Spaces
+            System.out.print(" ");
+        }
+
+        for(int j = 1; j <= i; j++){                 // Numbers
+            System.out.print(i  + " ");
+        }
+        System.out.println();
+    }
+}
 
     public static void main(String args[]){
 
-        diamondi(5);
+        numpy(9);
+
+        diamondi(7);
     }
 }
