ee464ca08f4ef25032efc1ec9dcd7a42c38ec581
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-10 13:46:05 +0530

    JavaPratice

--- Java/Arrays.java
+++ Java/Arrays.java
@@ -2,3 +2,35 @@
 
 - Array is a List of Elements of the same type placed in Contiguous memory Location. */
 
+
+// Creating an array
+
+//dataType arrayName[] = new dataType [Size]
+
+package Java;
+
+import javax.print.attribute.Size2DSyntax;
+
+public class Arrays{
+
+    public static void main(String args[]){
+
+        // int marks[] = new int [50];
+
+        int num[] = {1, 2 ,3};
+
+        int morenum[] = {4, 5, 6};
+
+        String Fruits[] = {"apple", "orange", "guava"};
+
+        System.out.println(num [0]);
+
+        System.out.println(morenum[1]);
+
+        System.out.println(Fruits[2]);
+        }
+    }
+
+
+
+
