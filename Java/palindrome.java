cc05923642d01e7e64973a3e52069a3c3b9e1422
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-12 08:49:11 +0530

    JavaPractice

--- Java/palindrome.java
+++ Java/palindrome.java
@@ -2,19 +2,34 @@ package Java;
 
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class palindrome {
 
 
-    public static void palindro(){
-        try(Scanner sc = new Scanner(System.in)){
-            String s1 = sc.nextLine();
+    public static void palindro(int num){
+        
 
+            int temp, rev = 0, Ld;
             
+            temp = num;
+            while(num != 0 ){
+                Ld = num % 10;
+                rev = (rev*10)+Ld;
+                num /= 10;
+            }
+
+            if(temp == rev){
+                System.out.println("Palindrome");
+            }else{
+                System.out.println("Not a palindrome");
+            }
         }
-    }
+    
 
     public static void main(String args[]){
-        palindro();
+        palindro(86);
+        
     }
     
 }
