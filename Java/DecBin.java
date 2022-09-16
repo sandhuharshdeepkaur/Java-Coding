16d4d54d0690f83cf5db9f80449c2cb1d85fd8f3
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-16 16:16:29 +0530

    JavaPractice

---
+++ Java/DecBin.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class DecBin {
+
+    // Program to print conversion of Binary number to Decimal number
+
+    public static void BintoDec(int binNum){
+        int myNum = binNum;
+        int pow = 0;
+        int dec = 0;
+
+        while(binNum>0){
+            int Ld = binNum % 10;
+            dec = dec + (Ld * (int)Math.pow(2, pow));
+            pow++;
+            binNum /= 10; 
+        }
+        System.out.println("Decimal number of "+ myNum + " = " + dec);
+    }
+    
+    public static void main(String args[]){
+        BintoDec(1001);
+    }
+}
