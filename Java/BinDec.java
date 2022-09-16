58d8fd09817e3f96757ea1d274218311f4cc17d7
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-16 17:48:39 +0530

    JavaPractice

--- Java/BinDec.java
+++ Java/BinDec.java
@@ -2,7 +2,7 @@ package Java;
 
 public class BinDec {
 
-    // Program to print conversion of Binary number to Decimal number
+    // Program to print Decimal number of given Binary number
 
     public static void BintoDec(int binNum){
         int myNum = binNum;
@@ -13,12 +13,36 @@ public class BinDec {
             int Ld = binNum % 10;
             dec = dec + (Ld * (int)Math.pow(2, pow));
             pow++;
-            binNum /= 10; 
+            binNum = binNum/10;
         }
         System.out.println("Decimal number of "+ myNum + " = " + dec);
     }
-    
+
+    //Program to print Binary number of given Decimal number
+    public static void DectoNum(int n){
+        
+        int myNum = n;
+        int pow = 0, binNum = 0;
+
+        while(n>0){
+            int rem = n%2;
+
+            binNum = binNum + (rem * (int) Math.pow(10,pow));
+            pow++;
+            
+            n /= 2;
+        }
+
+        System.out.println("Binary number of "+ myNum + " = " + binNum);
+    }
+
+
+
     public static void main(String args[]){
-        BintoDec(1001);
+
+        // BintoDec(1001);
+
+        DectoNum(7);
+
     }
 }
