75f2bc068e0040ed2b5fe1639d391cc78a0e3c0d
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-25 19:04:04 +0530

    JavaPractice

--- Java/OOPs.java
+++ Java/OOPs.java
@@ -1,12 +1,17 @@
 package Java;
 
-import java.util.Scanner;
 
 public class OOPs{
 
-
-    
 }
 
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        
+}
+}
 
 
