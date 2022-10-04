05fc77b2e96034e029604f59d16a66aebaa4e03f
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-10-04 15:21:23 +0530

    JavaPractice

--- Java/OOPs.java
+++ Java/OOPs.java
@@ -6,6 +6,8 @@ public class OOPs{
         Pen p1 = new Pen();
         p1.setColor("Blue");
         System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
     }
 
 }
@@ -16,7 +18,12 @@ class Pen{
 
     void setColor(String newColor){
         color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
 }
-}
+
 
 
