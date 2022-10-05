eab835859b3b41bc8f19c39f13994407e86bccd9
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-10-05 21:18:41 +0530

    JavaPractice

--- Java/OOPs.java
+++ Java/OOPs.java
@@ -4,16 +4,20 @@ package Java;
 public class OOPs{
     public static void main(String args[]){
         Pen p1 = new Pen();
+
         p1.setColor("Blue");
         System.out.println(p1.color);
         p1.setTip(5);
         System.out.println(p1.tip);
+        p1.setPoint("Ball");
+        System.out.println(p1.point);
     }
 
 }
 
 class Pen{
     String color;
+    String point;
     int tip;
 
     void setColor(String newColor){
@@ -22,6 +26,10 @@ class Pen{
 
     void setTip(int newTip){
         tip = newTip;
+    }
+
+    void setPoint(String newPoint){
+        point = newPoint;
     }
 }
 
