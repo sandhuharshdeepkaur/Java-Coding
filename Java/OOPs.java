60bbed176d4e2310185e79b09beb02f061da1dbc
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-10-06 12:40:09 +0530

    JavaPractice

--- Java/OOPs.java
+++ Java/OOPs.java
@@ -3,23 +3,33 @@ package Java;
 
 public class OOPs{
     public static void main(String args[]){
+        
+        //PEN Object
+
         Pen p1 = new Pen();
 
         p1.setColor("Blue");
         System.out.println(p1.color);
+
         p1.setTip(5);
         System.out.println(p1.tip);
+
         p1.setPoint("Ball");
         System.out.println(p1.point);
 
+        // Student Object
+        
         Student s1 = new Student();
 
         s1.setName("Monica");
         System.out.println("Student name: "+ s1.name);
+
         s1.Subjectname("Physics");
         System.out.println("Subject name: "+ s1.subject);
+        
         s1.Rollno(43);
         System.out.println("Student Roll no.: "+ s1.rollno);
+        
         s1.Marks(70);
         System.out.println("Physics marks: "+ s1.Submarks);
     }
